f8c81a33e7d5d05536d41cf7085fd1ef1f5b10ac
Sergio Ebanatti <sergioebanatti@gmail.com>
2023-02-08 00:36:03 +0700

    replaced array to list

--- Module_7/Program.cs
+++ Module_7/Program.cs
@@ -10,12 +10,12 @@ namespace Module_7
 {
     internal class Program
     {
-        private static string[] _workerTitles = Worker.WorkerMetaDataTitlesInit(); //Массив заголовков таблицы справочника.
-        private static Repository repository = new Repository("employeesDB.csv"); //Создание экземпляра класса репозиторий с указанием пути и имени файла.
+        private static string[] _workerTitles = Worker.WorkerMetaDataTitlesInit();      //Массив заголовков таблицы справочника.
+        private static Repository repository = new Repository("employeesDB.csv");       //Создание экземпляра класса репозиторий с указанием пути и имени файла.
 
         private static void Main()
         {
-            Console.SetWindowSize(200, 50); //Задание размера консольного окна, чтобы записи помещались без переносов строк.
+            Console.SetWindowSize(200, 50);     //Задание размера консольного окна, чтобы записи помещались без переносов строк.
             MainMenu();
             Console.ReadKey();
 
@@ -35,29 +35,52 @@ namespace Module_7
 
             if (char.ToLower(key) == '1')
             {
-                ReadAllWorkers(); //Вывод всех сотрудников.
+                PrintAllWorkers();      //Вывод всех сотрудников.
             }
             else if (char.ToLower(key) == '2')
             {
-                SearchWorkerById(); //Поиск сотрудника по ID.
+                SearchWorkerById();     //Поиск сотрудника по ID.
             }
             else if (char.ToLower(key) == '3')
             {
-                AddNewWorker(); //Добавление нового сотрудника.
+                AddNewWorker();     //Добавление нового сотрудника.
             }
             else if (char.ToLower(key) == '4')
             {
-                DeleteWorker(); //Удаление сотрудника.
+                DeleteWorker();     //Удаление сотрудника.
             }
             else if (char.ToLower(key) == '5')
             {
-                WorkersBetweenTwoDates(); //Вывод списка сотрудников в заданном диапазоне дат.
+               WorkersBetweenTwoDates();       //Вывод списка сотрудников в заданном диапазоне дат.
             }
         }
 
-        private static void ReadAllWorkers()
+        /// <summary>
+        /// Выводит данные обо всех сотрудниках на экран.
+        /// </summary>
+        private static void PrintAllWorkers()
         {
-            repository.PrintAllWorkersToConsole(); //Вывод всех сотрудников.
+            PrintTitles();
+            var workersToPrint = repository.GetAllWorkers();        //Формируем список сотрудников для вывода.
+
+            //СОРТИРОВКА!
+            /*workersToPrint.Sort(delegate (Worker one, Worker two)
+            {
+                if (one.FullName == null && two.FullName == null) return 0;
+                else if (one.FullName == null) return -1;
+                else if (two.FullName == null) return 1;
+                else return one.FullName.CompareTo(two.FullName);
+            });*/
+
+
+            for (int i = 0; i < repository.LinesCount; i++)     //Обходим список и конвертируем экземпляр Worker в строку для вывода на экран.
+            {
+                string resultToPrint = workersToPrint[i].PrintToConsole();
+                Console.WriteLine(resultToPrint);
+            }
+
+            Console.WriteLine(repository.LinesCount);       //УДАЛИТЬ ПОТОМ. счётчик строк
+
         }
 
         /// <summary>
@@ -73,9 +96,21 @@ namespace Module_7
                 var stringId = Console.ReadLine();
                 int id;
 
-                if (int.TryParse(stringId, out id)) //Проверяем, что введено число.
+                if (int.TryParse(stringId, out id))     //Проверяем, что введено число.
                 {
-                    repository.PrintSearchedWorkerToConsole(id); //Поиск сотрудника по ID.
+                    Worker searchedWorker = repository.GetWorkerById(id);       //Выполняем поиск сотрудника.
+                    string resultToPrint;
+
+                    if (repository.IsWorkerFound)
+                    {
+                        PrintTitles();
+                        resultToPrint = searchedWorker.PrintToConsole();
+                    }
+                    else
+                        resultToPrint = $"Сотрудник с ID {id} не найден";
+
+
+                    Console.WriteLine(resultToPrint);
                 }
                 else
                 {
@@ -85,15 +120,18 @@ namespace Module_7
 
             } while (char.ToLower(key) == 'д');
 
+
+
         }
+
         /// <summary>
         /// Добавление нового сотрудника.
         /// </summary>
         private static void AddNewWorker()
         {
-            char key = 'д';
+            char key = 'н';
 
-            do  //Цикл do для создания нескольких сотрудников подряд
+            do      //Цикл do для создания нескольких сотрудников подряд
             {
                 string note = String.Empty;
 
@@ -101,25 +139,25 @@ namespace Module_7
                 int height;
                 DateTime birthDate;
 
-                for (int index = 2; index < _workerTitles.Length; index++) /*Цикл для формирования записи о сотруднике.
-                                                                           Первые два индекса будут заполняться автоматически, поэтому начинается с 2.*/
+                for (int index = 2; index < _workerTitles.Length; index++)      /*Цикл для формирования записи о сотруднике.
+                                                                                Первые два индекса будут заполняться автоматически, поэтому начинается с 2.*/
                 {
-                    Console.Write($"{_workerTitles[index]}: "); //Вывод текущего заголовка, чтобы пользователь понимал, какие данные сейчас вводить.
-                    note += $"{Console.ReadLine()}#"; //Считывание строки + добавление # в качестве знака разделителя.
+                    Console.Write($"{_workerTitles[index].TrimStart(' ')}: ");      //Вывод текущего заголовка, чтобы пользователь понимал, какие данные сейчас вводить.
+                    note += $"{Console.ReadLine()}#";       //Считывание строки + добавление # в качестве знака разделителя.
                 }
 
-                string[] noteSplited = note.Split('#'); //Преобразование записи в массив с разделением по #.
+                string[] noteSplited = note.Split('#');     //Преобразование записи в массив с разделением по #.
 
                 if (!int.TryParse(noteSplited[1], out age) ||
                     !int.TryParse(noteSplited[2], out height) ||
-                    !DateTime.TryParse(noteSplited[3], out birthDate)) /*Минимальная проверка на корректность введённых данных.
-                                                                       Если что-то не так, то данные не запишутся.*/
+                    !DateTime.TryParse(noteSplited[3], out birthDate))      /*Минимальная проверка на корректность введённых данных.
+                                                                            Если что-то не так, то данные не запишутся.*/
                 {
                     Console.Write("Введены некорректные данные. Ещё раз? н/д\n"); key = Console.ReadKey(true).KeyChar;
                 }
                 else
                 {
-                    Worker newWorker = new Worker(noteSplited[0], age, height, birthDate, noteSplited[4]); //Выполяется создание экземпляра структуры с заполнением данных.
+                    Worker newWorker = new Worker(noteSplited[0], age, height, birthDate, noteSplited[4]);      //Выполяется создание экземпляра структуры с заполнением данных.
                     repository.AddWorker(newWorker);
                     Console.Write("Сотрудник добавлен. Продожить н/д\n");
                     key = Console.ReadKey(true).KeyChar;
@@ -171,10 +209,39 @@ namespace Module_7
             Console.Write("Введите дату до: ");
             DateTime.TryParse(Console.ReadLine(), out dateTo);
 
-            repository.PrintWorkersBetweenTwoDates(dateFrom, dateTo);
+            List<Worker> workersBetweenTwoDates = repository.GetWorkersBetweenTwoDates(dateFrom, dateTo);       //Формируем список сотрудников, подходящих по условию.
+            string resultToPrint;
+
+            if (repository.IsWorkerFound)
+            {
+                PrintTitles();
+                for (int i = 0; i < workersBetweenTwoDates.Count; i++)
+                {
+                    resultToPrint = workersBetweenTwoDates[i].PrintToConsole(); //Конвертируем список сотрудников в строку для вывода на экран.
+                    Console.WriteLine(resultToPrint);
+                }
+            }
+            else
+            {
+                resultToPrint = $"Не найдено сотрудников, созданных между {dateFrom.ToShortDateString()} и {dateTo.ToShortDateString()}";
+                Console.WriteLine(resultToPrint);
+            }
         }
 
-   
+        /// <summary>
+        /// Выводит на экран заголовки.
+        /// </summary>
+        private static void PrintTitles()
+        {
+            foreach (var title in _workerTitles)
+            {
+                Console.Write(title);
+            }
+
+            Console.WriteLine("\n");
+        }
+
+
 
     }
 }
--- Module_7/Repository.cs
+++ Module_7/Repository.cs
@@ -14,21 +14,27 @@ namespace Module_7
     /// </summary>
     public class Repository
     {
-        private static string[] _workerTitles = Worker.WorkerMetaDataTitlesInit(); //Массив заголовков таблицы справочника.
+        #region Поля
 
-        private Worker[] _workers; // Основной массив для хранения данных.
-        private int _linesCount = 0; //Текущее количество записей в файле на диске.
-        private bool _isWorkerFound;
+        private static string[] _workerTitles = Worker.WorkerMetaDataTitlesInit();      //Массив заголовков таблицы справочника.
+        private List<Worker> _workers;      // Основной список для хранения данных.
         private bool _isWorkerDeleted;
 
+        #endregion
+
         #region Свойства
+
         /// <summary>
         /// Имя и путь файла с данными.
         /// </summary>
-        public string RepositoryPathName { get; set; }
+        public string RepositoryPathName { get; set; }      //Путь и имя файла на диске.
+        public int LinesCount { get; set; }     //Текущее количество записей в файле на диске.
+        public bool IsWorkerFound { get; set; }
+
         #endregion
 
         #region Конструктор
+
         /// <summary>
         /// Создание репозиория с указанием пути и имени файла.
         /// </summary>
@@ -36,117 +42,63 @@ namespace Module_7
         public Repository(string repositoryPathName)
         {
             RepositoryPathName = repositoryPathName;
-            _workers = new Worker[1];
-            _workers = GetAllWorkers(); //Начальное заполнение массива данными.
+            _workers = new List<Worker>();
+            _workers = GetAllWorkers();     //Начальное заполнение списка данными.
         }
+
         #endregion
 
         #region Методы
 
         /// <summary>
-        /// Выводит на экран таблицу данных обо всех сотрудниках.
+        /// Считывает все записи из файла на диске и записывает их в список.
         /// </summary>
-        public void PrintAllWorkersToConsole()
+        /// <returns>Список экземпляров Worker</returns>
+        public List<Worker> GetAllWorkers()
         {
-            Console.WriteLine($"\n{_workerTitles[0]}" +
-                $" {_workerTitles[1]}" +
-                $" {_workerTitles[2]}" +
-                $" {_workerTitles[3]}" +
-                $" {_workerTitles[4]}" +
-                $" {_workerTitles[5]}" +
-                $" {_workerTitles[6]}\n");  //Вывод заголовков.
-
-            Worker[] workersToPrint = GetAllWorkers(); //Формируем массив сотрудников для вывода.
-            Array.Resize(ref workersToPrint, _linesCount);
-
-            var list = workersToPrint.ToList();
-            //var orderedElem = workersToPrint.OrderBy(w => w.FullName);
-            list.Sort(delegate (Worker one, Worker two)
+            if (IsRepositoryExist())
             {
-                if (one.FullName == null && two.FullName == null) return 0;
-                else if (one.FullName == null) return -1;
-                else if (two.FullName == null) return 1;
-                else return one.FullName.CompareTo(two.FullName);
-            });
-
-            //workersToPrint = list.ToArray();
-
-
-            for (int i = 0; i < _linesCount; i++)
-            {
-                string resultToPrint = list[i].PrintToConsole();  //Конвертируем экземпляр Worker в строку для вывода на экран.
-                Console.WriteLine(resultToPrint);
-            }
-
-            for (int i = 0; i < _linesCount; i++)
-            {
-                
-            }
-
-        }
-
-        /// <summary>
-        /// Выводит на экран искомого сотрудника.
-        /// </summary>
-        /// <param name="id">ID, который требуется найти</param>
-        public void PrintSearchedWorkerToConsole(int id)
-        {
-            Worker searchedWorker = GetWorkerById(id); //Выполняем поиск сотрудника.
-            string resultToPrint;
-
-            if (_isWorkerFound)
-            {
-                Console.WriteLine($"\n{_workerTitles[0]}" +
-                    $" {_workerTitles[1]}" +
-                    $" {_workerTitles[2]}" +
-                    $" {_workerTitles[3]}" +
-                    $" {_workerTitles[4]}" +
-                    $" {_workerTitles[5]}" +
-                    $" {_workerTitles[6]}\n");  //Вывод заголовков.
-
-                resultToPrint = searchedWorker.PrintToConsole();
-            }
-            else
-            {
-                resultToPrint = $"Сотрудник с ID {id} не найден";
-            }
-
-            Console.WriteLine(resultToPrint);
-
-        }
-
-        /// <summary>
-        /// Выводит на экран записи за заданный период времени.
-        /// </summary>
-        /// <param name="dateFrom">С даты</param>
-        /// <param name="dateTo">До даты</param>
-        public void PrintWorkersBetweenTwoDates(DateTime dateFrom, DateTime dateTo)
-        {
-            Worker[] workersBetweenTwoDates = GetWorkersBetweenTwoDates(dateFrom, dateTo);  //Формируем массив данных.
-            string resultToPrint;
-
-            if (_isWorkerFound)
-            {
-                Console.WriteLine($"{_workerTitles[0]}" +
-                    $" {_workerTitles[1],25}" +
-                    $" {_workerTitles[2],25}" +
-                    $" {_workerTitles[3],25}" +
-                    $" {_workerTitles[4],15}" +
-                    $" {_workerTitles[5],25}" +
-                    $" {_workerTitles[6],25}\n");   //Вывод заголовков.
-
-                for (int i = 0; i < workersBetweenTwoDates.Length; i++)
+                using (StreamReader streamReader = new StreamReader(RepositoryPathName))
                 {
-                    resultToPrint = workersBetweenTwoDates[i].PrintToConsole(); //Конвертируем массив Worker[] в строку для вывода на экран.
-                    Console.WriteLine(resultToPrint);
+                    LinesCount = 0;     //Сбрасываем счётчик строк т.к. будем считывать все записи из файла.
+
+                    while (!streamReader.EndOfStream)
+                    {
+                        string[] workerLine = streamReader.ReadLine().Split('#');       //Считываем одну строку и разбиваем её на элементы разделителем #.
+
+                        LinesCount++;
+                        _workers.Add(new Worker(
+                            Convert.ToInt32(workerLine[0]),
+                            Convert.ToDateTime(workerLine[1]),
+                            workerLine[2],
+                            Convert.ToInt32(workerLine[3]),
+                            Convert.ToInt32(workerLine[4]),
+                            Convert.ToDateTime(workerLine[5]),
+                            workerLine[6]));        //Конвертируем массив строк в элемент списка Worker.
+                    }
                 }
             }
-            else
-            {
-                resultToPrint = $"Не найдено сотрудников, созданных между {dateFrom.ToShortDateString()} и {dateTo.ToShortDateString()}";
-                Console.WriteLine(resultToPrint);
-            }
+            return _workers;
+        }
 
+        /// <summary>
+        /// Происходит чтение из файла, возвращается Worker с запрашиваемым ID.
+        /// </summary>
+        /// <param name="id">ID, который требуется найти</param>
+        /// <returns>Искомый экземпляр Worker</returns>
+        public Worker GetWorkerById(int id)
+        {
+            Worker searchedWorker;
+            foreach (var item in _workers)      //Ищем сотрудника в списке.
+            {
+                if (item.Id == id)
+                {
+                    IsWorkerFound = true;
+                    return searchedWorker = item;
+                }
+            }
+            IsWorkerFound = false;
+            return new Worker();        //Если сотрудник не найден, возвращаем пустую запись.
         }
 
         /// <summary>
@@ -157,12 +109,12 @@ namespace Module_7
         {
             using (StreamWriter streamWriter = new StreamWriter(RepositoryPathName, true))
             {
-                worker.Id = _workers[_linesCount - 1].Id + 1;   /*Формируем ID на основе ID последнего записанного сотрудника.
+                worker.Id = _workers[LinesCount - 1].Id + 1;   /*Формируем ID на основе ID последнего записанного сотрудника.
                                                                 _linesCount - 1 т.к. счётчик больше индекса на 1.*/
                 worker.CreateDateTime = DateTime.Now;
                 string note = worker.PrintToWriteFile();    //Конвертируем Worker в строку, для записи в файл.
                 streamWriter.WriteLine(note);
-                _linesCount++;
+                LinesCount++;
             }
         }
 
@@ -172,28 +124,23 @@ namespace Module_7
         /// <param name="id">ID удаляемого сотрудника</param>
         public void DeleteWorker(int id)
         {
-            var workersList = _workers.ToList();    //Конвертируем массив в список для удобства.
-
-            for (int i = 0; i < _linesCount; i++)
+            for (int i = 0; i < LinesCount; i++)
             {
-                if (workersList[i].Id == id)    //Обходим список и сравниваем с искомым ID.
+                if (_workers[i].Id == id)    //Обходим список и сравниваем с искомым ID.
                 {
-                    workersList.RemoveAt(i);    //Удаляем элемент.
-                    _workers = workersList.ToArray();  //действие под вопросом??
-                    _linesCount--;
+                    _workers.RemoveAt(i);    //Удаляем элемент.
+                    LinesCount--;
                     _isWorkerDeleted = true;
                     break;
-
                 }
             }
-
 
             if (_isWorkerDeleted)
             {
                 string note = String.Empty;
 
-                for (int i = 0; i < _linesCount; i++)
-                    note += $"{workersList[i].PrintToWriteFile()}\n";   /*Обходим список и конвертируем его в строчный тип записи, согласно формату хранения на диске,
+                for (int i = 0; i < LinesCount; i++)
+                    note += $"{_workers[i].PrintToWriteFile()}\n";   /*Обходим список и конвертируем его в строчный тип записи, согласно формату хранения на диске,
                                                                         в каждой строке данные по одному сотруднику.*/
 
                 string[] resultString = note.Split('\n'); //    Разделяем записи построчно и записываем каждую строку в отдельный элемент массива.
@@ -209,117 +156,27 @@ namespace Module_7
         }
 
         /// <summary>
-        /// Считывает все записи из файла на диске и записывает их в массив.
-        /// </summary>
-        /// <returns>массив экземпляров Worker</returns>
-        private Worker[] GetAllWorkers()
-        {
-
-            if (IsRepositoryExist())
-            {
-                using (StreamReader streamReader = new StreamReader(RepositoryPathName))
-                {
-                    _linesCount = 0; //Сбрасываем счётчик строк т.к. будем считывать все записи из файла.
-
-                    while (!streamReader.EndOfStream)
-                    {
-                        string[] workerLine = streamReader.ReadLine().Split('#'); //Считываем одну строку и разбиваем её на элементы разделителем #.
-
-                        _linesCount++;
-                        Resize(_linesCount >= _workers.Length);
-
-                        _workers[_linesCount - 1] = new Worker(
-                            Convert.ToInt32(workerLine[0]),
-                            Convert.ToDateTime(workerLine[1]),
-                            workerLine[2],
-                            Convert.ToInt32(workerLine[3]),
-                            Convert.ToInt32(workerLine[4]),
-                            Convert.ToDateTime(workerLine[5]),
-                            workerLine[6]); //Конвертируем массив строк в элемент массива Worker[].
-
-                        //_linesCount++;
-
-                    }
-                    //_linesCount--;
-                }
-
-
-            }
-
-            return _workers;
-
-        }
-
-        /// <summary>
-        /// Происходит чтение из файла, возвращается Worker с запрашиваемым ID.
-        /// </summary>
-        /// <param name="id">ID, который требуется найти</param>
-        /// <returns>Искомый экземпляр Worker</returns>
-        private Worker GetWorkerById(int id)
-        {
-            Worker searchedWorker;
-
-            foreach (var item in _workers)  //Ищем сотрудника в хранилище.
-            {
-                if (item.Id == id)
-                {
-                    _isWorkerFound = true;
-                    return searchedWorker = item;
-                }
-            }
-
-            _isWorkerFound = false;
-            return new Worker();    //Если сотрудник не найден, возвращаем пустую запись.
-
-
-        }
-
-        /// <summary>
         /// Получет данные о сотрудниках за определённый период времени.
         /// </summary>
         /// <param name="dateFrom">С даты</param>
         /// <param name="dateTo">До даты</param>
         /// <returns>Массив Worker[] за искомый период</returns>
-        private Worker[] GetWorkersBetweenTwoDates(DateTime dateFrom, DateTime dateTo)
+        public List<Worker> GetWorkersBetweenTwoDates(DateTime dateFrom, DateTime dateTo)
         {
+            List<Worker> workersBetweenTwoDates = new List<Worker>();       //Создаем дополнительный список, в который будут помещены подходящие по условию сотрудники.
+            IsWorkerFound = false;
 
-            Worker[] workersBetweenTwoDates = new Worker[_linesCount];  /*Инициализируем дополнительный массив элементов в который будут записаны искомые данные
-                                                                        Длинну массива пока указываем по счётчику записей из файла*/
-
-            int additionalIndex = 0;    //Счётчик дополнительного массива
-            _isWorkerFound = false;
-
-            for (int mainIndex = 0; mainIndex < _linesCount; mainIndex++)   //Обходим основной массив и ищем нужные элементы.
+            for (int i = 0; i < LinesCount; i++)   //Обходим основной список и ищем нужные элементы.
             {
-                if (_workers[mainIndex].CreateDateTime >= dateFrom && _workers[mainIndex].CreateDateTime <= dateTo)
+                if (_workers[i].CreateDateTime >= dateFrom && _workers[i].CreateDateTime <= dateTo)
                 {
-                    workersBetweenTwoDates[additionalIndex] = _workers[mainIndex];  //Записываем в дополнительный массив найденный элемент из основного массива.
-                    _isWorkerFound = true;
-                    additionalIndex++;
+                    workersBetweenTwoDates.Add(_workers[i]);        //Записываем в дополнительный список найденный элемент из основного списка.
+                    IsWorkerFound = true;
                 }
             }
-
-            Array.Resize(ref workersBetweenTwoDates, additionalIndex);  //Меняем размер массива по счётчику дополнительного массива.
-
             return workersBetweenTwoDates;
         }
 
-        /// <summary>
-        /// Увеличиваем, если требуется, размер массива данных.
-        /// </summary>
-        /// <param name="Flag">Условие увеличения</param>
-        private void Resize(bool Flag)
-        {
-            if (Flag)
-            {
-                Array.Resize(ref _workers, _workers.Length * 2); //Увеличиваем размер массива в 2 раза.
-            }
-        }
-
-        /// <summary>
-        /// Создан ли файл?
-        /// </summary>
-        /// <returns></returns>
         private bool IsRepositoryExist()
         {
             if (File.Exists(RepositoryPathName))
@@ -330,6 +187,7 @@ namespace Module_7
             Console.WriteLine("Сотрудники отсутствуют");
             return false;
         }
+
         #endregion
     }
 }
